c1fe47357460ae00062aaba354b8aedf804109c4
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-26 18:25:49 -0800

    hw63

--- routes/routes.py
+++ routes/routes.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
 from controllers.customerController import save, find_all
-from controllers.productController import save
+
 
 customer_blueprint = Blueprint('customer_bp', __name__)
 customer_blueprint.route('/', methods=['POST'])(save)
 customer_blueprint.route('/', methods=['GET'])(find_all)
 
 
-product_blueprint = Blueprint('product_bp', __name__)
-product_blueprint.route('/', methods=['POST'])(save)
