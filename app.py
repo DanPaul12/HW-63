b539f9c65db690517890ffac2666eddeaaa75f4d
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-27 12:06:02 -0800

    hw63

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.productBP import product_blueprint
 from routes.employeeBP import employee_blueprint
 from routes.orderBP import order_blueprint
 from routes.productionBP import production_blueprint
+from limiter import limiter
 from schema import ma
 
 
@@ -24,12 +25,15 @@ def blue_print_config(app):
     app.register_blueprint(order_blueprint, url_prefix='/orders')
     app.register_blueprint(production_blueprint, url_prefix='/productions')
 
+def configure_rate_limit():
+    limiter.limit('5 per day')(customer_blueprint)
 
 
 if __name__ == '__main__':
     app = create_app('DevelopmentConfig')
 
     blue_print_config(app)
+    configure_rate_limit()
 
     with app.app_context():
         db.drop_all()
