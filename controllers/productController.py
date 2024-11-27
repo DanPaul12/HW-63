6f23a5d1061d9dbcbf9937783fa9f93dde61fe5b
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-26 18:22:39 -0800

    hw63

--- controllers/productController.py
+++ controllers/productController.py
@@ -5,15 +5,15 @@ from flask import request, jsonify
 
 def save():
     try:
-        product = product_schema.load(request.json)
-    except ValidationError:
-        pass
-
-    product_save = productService.save(product)
-    if product_save is not None:
-        return jsonify(product_save), 201
-    else:
-        return jsonify('error')
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    try:
+        product_save = productService.save(product_data)
+        return product_schema.jsonify(product_save), 201
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
 
 def find_all():
     products = productService.find_all()
