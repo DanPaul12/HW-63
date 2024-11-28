263133c5de9cad31cf0d7e93dc10e197ff98c106
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-28 14:03:00 -0800

    hw63

--- controllers/orderController.py
+++ controllers/orderController.py
@@ -10,3 +10,8 @@ def save():
 def find_all():
     orders = orderService.find_all()
     return orders_schema.jsonify(orders), 200
+
+def find_all_pagination():
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+    return orders_schema.jsonify(orderService.find_all_pagination(page=page, per_page=per_page))
