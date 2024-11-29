e7da047bc386c3884b1d49befe43eaba45a09b6e
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-28 16:20:18 -0800

    hw63

--- routes/productBP.py
+++ routes/productBP.py
@@ -4,4 +4,4 @@ from controllers.productController import save, find_all, find_all_pagination
 product_blueprint = Blueprint('product_bp', __name__)
 product_blueprint.route('/', methods=['POST'])(save)
 product_blueprint.route('/', methods=['GET'])(find_all)
-product_blueprint.route('/pagination', methods=['GET'])(find_all_pagination)
+product_blueprint.route('/paginate', methods=['GET'])(find_all_pagination)
